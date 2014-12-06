a1dd3045e8dec0f493d74890de286152d0ded639
ParleyMartins <ParleyMartins@users.noreply.github.com>
2014-12-06 09:11:13 -0200

    Update teste-build.sh

--- teste-build.sh
+++ teste-build.sh
@@ -1,12 +1,10 @@
 cd ~/chef-repo
 sudo chef-solo -c solo.rb -j web.json
 
-
 cd ~/workspace/noosfero
 sed -i '/NOOSFERO_DIR=\/var\/lib\/noosfero\/current/c\NOOSFERO_DIR=/var/lib/jenkins/workspace/noosfero ' ./etc/init.d/noosfero
 sed -i '/NOOSFERO_USER=noosfero/c\NOOSFERO_USER=jenkins' ./etc/init.d/noosfero
 sudo ./etc/init.d/noosfero setup
-
 
 sudo bundle --local
 
@@ -23,13 +21,15 @@ test:
   username: jenkins" > config/database.yml
 
 export RAILS_ENV=test
+rake db:create
 rake db:schema:load
-rake noosfero:translations:compile >/dev/null 2>&1
 rake db:migrate
+rake noosfero:translations:compile >/dev/null 2>&1
 rake db:test:prepare
 rake db:drop
 
 export RAILS_ENV=prodution
+rake db:create
 rake db:schema:load
 rake noosfero:translations:compile >/dev/null 2>&1
 rake db:migrate
