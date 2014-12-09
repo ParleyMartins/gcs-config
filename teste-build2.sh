ff997f465bf4fce116909f57c1061500d10493e7
ParleyMartins <ParleyMartins@users.noreply.github.com>
2014-12-09 08:55:57 -0200

    Update teste-build2.sh

--- teste-build2.sh
+++ teste-build2.sh
@@ -14,11 +14,6 @@ export RAILS_ENV=prodution
 rake db:create
 rake db:schema:load
 rake noosfero:translations:compile >/dev/null 2>&1
-rails c <<EOF
-Environment.create!(:name => 'Noosfero', :contact_email => 'noosfero@localhost.localdomain', :is_default => true)
-Environment.default.domains << Domain.new(:name => 104.131.171.49)
-user = User.create(:login => 'adminuser', :email => 'admin@example.com', :password => 'admin', :password_confirmation => 'admin', :environment => Environment.default, :activated_at => Time.new)
-user.activate
-EOF
+
 ./script/production start
 ./script/production stop
