030428ab138c94a2cf5c5544e4a55e805e0af8f2
ParleyMartins <ParleyMartins@users.noreply.github.com>
2014-12-09 10:52:54 -0200

    Update config-jenkins.sh

--- config-jenkins.sh
+++ config-jenkins.sh
@@ -6,7 +6,7 @@ COLOR_END="\033[m\n"
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install -y git jenkins
+sudo apt-get install -y git jenkins vim
 
 wget http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin Git gitlab-plugin Rake -restart
