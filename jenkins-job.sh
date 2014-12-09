496a3e8239477ca74a46774eee889cb1fdffdb60
ParleyMartins <ParleyMartins@users.noreply.github.com>
2014-12-09 15:06:46 -0200

    Create jenkins-job.sh

---
+++ jenkins-job.sh
@@ -0,0 +1,16 @@
+IPS=$(hostname -I)
+LOCAL_IP=($(echo ${IPS}))
+
+wget http://$LOCAL_IP:8080/jnlpJars/jenkins-cli.jar
+java -jar jenkins-cli.jar -s http://$LOCAL_IP:8080/ install-plugin git
+java -jar jenkins-cli.jar -s http://$LOCAL_IP:8080/ install-plugin gitlab-plugin
+java -jar jenkins-cli.jar -s http://$LOCAL_IP:8080/ install-plugin rake -restart
+java -jar jenkins-cli.jar -s http://$LOCAL_IP:8080/ create-job noosfero < ~/gcs-config/config.xml
+java -jar jenkins-cli.jar -s http://$LOCAL_IP:8080/ build noosfero
+sudo service jenkins restart
+
+sleep 5m
+sudo su jenkins <<EOF
+cd ~/jobs/noosfero/workspace/
+./script/quick-start
+EOF
