17d55635a442ee6a0f1168d7e2a84526ced902e8
ParleyMartins <ParleyMartins@users.noreply.github.com>
2014-12-10 13:12:27 -0200

    Update config-apache.sh

--- config-apache.sh
+++ config-apache.sh
@@ -1,8 +1,6 @@
 sudo a2enmod deflate expires proxy proxy_balancer proxy_http rewrite
 sudo service apache2 restart
 
-sudo cp  ~/gcs-config/noosfero /etc/apache2/sites-available/noosfero
-
 sudo a2ensite noosfero
 sudo service apache2 reload
 sudo invoke-rc.d apache2 restart
